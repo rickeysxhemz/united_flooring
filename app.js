98c039cf7ddc87d422df4ef5d5613fe135ad1a6d
rickeysxhemz <rickeysxhemz@gmail.com>
2023-10-05 10:57:19 -0400

    minor changes

--- app.js
+++ app.js
@@ -3,6 +3,13 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
+app.get('/', (req, res) => {
+  res.send('nodemon test!');
+});
+app.get('/hello',(req,res)=>{
+  res.send('Hello World!');
+}
+);
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
